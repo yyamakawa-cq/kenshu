778aec737184c256ffb7a9125cb2b5a4a4177097
yyamakawa-cq <y.yamakawa@caraquri.com>
2017-04-24 16:44:44 +0900

    Initial Commit

---
+++ kenshu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kenshu
+//
+//  Created by yukari on 2017/04/24.
+//  Copyright © 2017年 yukari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
